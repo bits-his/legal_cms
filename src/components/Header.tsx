597476edbc4b104a6ec2810f205d2e1a93cb58e3
ahmadkabir <atomakabir001@gmail.com>
2025-10-31 16:32:50 +0100

    save

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ const { Header: AntHeader } = Layout;
 const Header: React.FC = () => {
   return (
     <AntHeader className="glass bg-white/25 backdrop-blur-lg shadow-lg border-b-2 border-green-600 px-4 py-3 lg:px-12">
-      <h1 className="text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold text-green-800">
-        {/* Auwal I Magashi & Co. */}
-      </h1>
+      <h3 className="text-center sm:text-2xl md:text-4xl lg:text-3xl font-semibold text-green-800">
+        PASmartSuite
+      </h3>
     </AntHeader>
   );
 };
