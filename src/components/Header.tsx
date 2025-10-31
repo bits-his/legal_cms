ca7b9801522f8bd30503757eb8ed0c9c375b2bd8
ahmadkabir <atomakabir001@gmail.com>
2025-10-31 18:19:00 +0100

    save

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ const { Header: AntHeader } = Layout;
 const Header: React.FC = () => {
   return (
     <AntHeader className="glass bg-white/25 backdrop-blur-lg shadow-lg border-b-2 border-green-600 px-4 py-3 lg:px-12">
-      <h3 className="text-center sm:text-2xl md:text-4xl lg:text-3xl font-semibold text-green-800">
+      <h1 className="text-center text-4xl sm:text-4xl md:text-5xl lg:text-5xl font-semibold text-green-800">
         PASmartSuite
-      </h3>
+      </h1>
     </AntHeader>
   );
 };
